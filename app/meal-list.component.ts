d0c49f652fdb5204876ba5092d0f00bfb7f763c0
Gray Hildreth <grayhildreth@gmail.com>
2016-10-27 00:37:13 -0700

    create pipe.ts file

--- app/meal-list.component.ts
+++ app/meal-list.component.ts
@@ -4,6 +4,12 @@ import { Meal } from './meal.model';
 @Component({
   selector: 'meal-list',
   template: `
+  <select (change)="onChange($event.target.value)">
+    <option value="all">Show All</option>
+    <option value="under500">Under 500 Calories</option>
+    <option value="over500" selected="selected">Over 500 Calories</option>
+  </select>
+
   <div *ngFor="let currentMeal of childMealList">
     <h3>Meal: {{ currentMeal.description }}</h3>
     <h4>Details: {{ currentMeal.details }}</h4>
