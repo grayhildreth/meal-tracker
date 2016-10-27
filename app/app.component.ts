eb548d37caf1848ad56158b62642f4c9ebae9000
Gray Hildreth <grayhildreth@gmail.com>
2016-10-26 19:28:03 -0700

    build app.components.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 
-@
-Component({
-    selector: 'my-app',
-    template: `
-    <div class="container">
-      <h1>Meal Tracker</h1>
-    </div>
-
+@Component({
+  selector: 'my-app',
+  template: `
+  <div class="container">
+    <h1>Meal Tracker</h1>
+    <h3 *ngFor="let currentMeal of meals">{{ currentMeal.food }}</h3>
+  </div>
   `
 })
-export class AppComponent {
 
+export class AppComponent {
+  public meals: Meal[] = [
+      new Meal("Burrito", 560),
+      new Meal("Slice of Pizza", 250),
+      new Meal("Pasta", 400)
+  ];
+}
+
+export class Meal {
+  public done: boolean = false;
+  constructor(public food: string, public calories: number) {   }
 }
